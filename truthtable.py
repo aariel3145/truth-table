89791f4714fce470adf244b8100598f704ccb1aa
Amy Ariel <aariel3145@gmail.com>
2021-09-09 21:20:19 -0400

    Revamped truth tables Now 1-5 variables getOutput function has optional arguments

--- truthtable.py
+++ truthtable.py
@@ -8,25 +8,53 @@ Description: Generates a truth table for a user-defined boolean expression
 Programmer: Amy Ariel
 """
 
-def getOutput(a, b, c, d):
-    f = (a and c) or (((not a) or b) and (not c) and (not d))
+def getOutput(e, d='x', c='x', b='x', a='x'):   # LSB (e) first since we have optional arguments
+    f = not(e)
     
-    print(f"{a} {b} {c} {d} | {int(f)}")
+    print(f"{a} {b} {c} {d} {e} | {int(f)}")
 
-print(f"A B C D | F")
-getOutput(0, 0, 0, 0)
-getOutput(0, 0, 0, 1)
-getOutput(0, 0, 1, 0)
-getOutput(0, 0, 1, 1)
-getOutput(0, 1, 0, 0)
-getOutput(0, 1, 0, 1)
-getOutput(0, 1, 1, 0)
-getOutput(0, 1, 1, 1)
-getOutput(1, 0, 0, 0)
-getOutput(1, 0, 0, 1)
-getOutput(1, 0, 1, 0)
-getOutput(1, 0, 1, 1)
-getOutput(1, 1, 0, 0)
-getOutput(1, 1, 0, 1)
-getOutput(1, 1, 1, 0)
-getOutput(1, 1, 1, 1)
+while(True):
+    numVar = int(input("Number of variables? "))
+
+    if numVar == 5:
+        print(f"A B C D E | F")
+        for i in range(2):
+            for j in range(2):
+                for k in range(2):
+                    for l in range(2):
+                        for m in range(2):
+                            getOutput(m, l, k, j, i)
+        break
+
+    elif numVar == 4:
+        print(f"- A B C D | F")
+        for i in range(2):
+            for j in range(2):
+                for k in range(2):
+                    for l in range(2):
+                            getOutput(l, k, j, i)
+        break
+
+    elif numVar == 3:
+        print(f"- - A B C | F")
+        for i in range(2):
+            for j in range(2):
+                for k in range(2):
+                            getOutput(k, j, i)
+        break
+
+    elif numVar == 2:
+        print(f"- - - A B | F")
+        for i in range(2):
+            for j in range(2):
+                            getOutput(j, i)
+        break
+
+    elif numVar == 1:
+        print(f"- - - - A | F")
+        for i in range(2):
+                            getOutput(i)
+        break
+    
+    else:
+        print(f"Enter a number 1 thru 5.")
